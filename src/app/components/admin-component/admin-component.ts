0b201035cb494372570479fdf7afe8deaf1dac0e
Chukwu Sydney <chukwusydney061@gmail.com>
2025-11-06 04:16:43 +0100

    Show What a Component Is

---
+++ src/app/components/admin-component/admin-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-component',
+  imports: [],
+  templateUrl: './admin-component.html',
+  styleUrl: './admin-component.css',
+})
+export class AdminComponent {
+   firstName: "Chukwu";
+
+   courseName: "Videography";
+
+   rollNo: 123456;
+}
