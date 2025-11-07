7e107f9b85c8ca19b55d083c9931adbb07f37780
Chukwu Sydney <chukwusydney061@gmail.com>
2025-11-07 09:55:20 +0100

    Implement Routing Practices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,27 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin-component/admin-component';
+import { ControlFlowComponent } from './components/control-flow-component/control-flow-component';
+import { DatabindingComponent } from './components/databinding-component/databinding-component';
+import { SignalComponent } from './components/signal-component/signal-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'control-flow-statement',
+    component: ControlFlowComponent
+  },
+
+  {
+    path: 'databinding',
+    component: DatabindingComponent
+  },
+
+  {
+    path: 'signal',
+    component: SignalComponent
+   },
+];
 // handles navigation or to achieve page based navigation
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, } from '@angular/router';
 import { UserComponent } from "./components/user-component/user-component";
 import { AdminComponent } from './components/admin-component/admin-component';
 import { DatabindingComponent } from './components/databinding-component/databinding-component';
 import { SignalComponent } from './components/signal-component/signal-component';
+import { ControlFlowComponent } from './components/control-flow-component/control-flow-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent, AdminComponent, DatabindingComponent, SignalComponent],
+  imports: [RouterOutlet, UserComponent, AdminComponent, DatabindingComponent, SignalComponent,ControlFlowComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
